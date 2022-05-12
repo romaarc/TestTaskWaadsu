dd061754deb63b1b2678e773cd96026892763bbe
Roman <romangorshkovreme@gmail.com>
2022-05-12 16:09:43 +0300

    Create project

---
+++ TestTaskWaadsu/TestTaskWaadsu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestTaskWaadsu
+//
+//  Created by Roman Gorshkov on 12.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .magenta
+    }
+
+
+}
+
